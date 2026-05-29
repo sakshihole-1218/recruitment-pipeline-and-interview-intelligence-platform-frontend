62d3f583ac97b7ea534bbddd9e0abec4881b137e
Sakshi <sakshi.h@aquillai.io>
2026-05-29 13:34:20 +0530

    chore: setup shared api response types

---
+++ src/shared/types/api.types.ts
@@ -0,0 +1,12 @@
+export interface ApiSuccessResponse<T> {
+  success: true;
+  message: string;
+  data: T;
+}
+
+export interface ApiErrorResponse {
+  success: false;
+  message: string;
+  error?: unknown;
+  statusCode?: number;
+}
